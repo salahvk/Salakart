f3022c2d392ad37009b863566153f971b5fd8f8b
Salah <salah@viewydigital.com>
2024-02-29 22:12:36 +0530

    customer and product models created

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer (models.Model):
+    Live = 1
+    DELETE = 0
+    DELETE_CHOICES = ((Live,'Live'),(DELETE,'Delete'))
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone = models.CharField(max_length=10)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name= 'customer_profile')
+    delete_status = models.IntegerField(choices=DELETE_CHOICES,default = Live)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name
--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product (models.Model):
+    Live = 1
+    DELETE = 0
+    DELETE_CHOICES = ((Live,'Live'),(DELETE,'Delete'))
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='/media')
+    priority = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES,default = Live)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.name
