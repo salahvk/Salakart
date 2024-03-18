15aaa43d9a804f3132cc531f0f052c9762dd8cc5
salahvk <salahkoyilandy@gmail.com>
2024-03-18 22:56:04 +0530

    customer,product,order models created

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Product (models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     description = models.TextField()
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     priority = models.IntegerField(default=0)
     delete_status = models.IntegerField(choices=DELETE_CHOICES,default = Live)
     created_at = models.DateTimeField(auto_now_add=True)
