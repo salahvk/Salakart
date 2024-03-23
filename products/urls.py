2dc24ba49da3f519dcbef127cf136beb28743421
salahvk <salahkoyilandy@gmail.com>
2024-03-23 18:28:59 +0530

    layout design modified

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='home'),
+    path('product_list',views.list_products,name='list_product')
 ]
--- products/views.py
+++ products/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def index(request) :
     return render(request,'index.html')
+
+def list_products(request) :
+    return render(request, 'products.html')
+
+def detail_product(request) :
+    return render(request, 'product_detail.html')
+
